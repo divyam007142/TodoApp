5079778fd0951304a5133bcbcd67fdff3713b84e
divyam007142 <divyamsingha22@gmail.com>
2025-06-06 11:58:49 +0530

    Add files via upload

---
+++ src/utils/index.ts
@@ -0,0 +1,16 @@
+export { exportTasksToJson } from "./exportTasks";
+export { getFontColor, isDark, isHexColor } from "./colorUtils";
+export { getRandomGreeting } from "./getRandomGreeting";
+export { systemInfo } from "./getSystemInfo";
+export { saveQRCode } from "./saveQRCode";
+export { showToast } from "./showToast";
+export { generateUUID } from "./generateUUID";
+export { timeAgo, formatDate, calculateDateDifference } from "./timeUtils";
+export {
+  initDB,
+  deleteProfilePictureFromDB,
+  fileToBase64,
+  getProfilePictureFromDB,
+  saveProfilePictureInDB,
+  validateImageFile,
+} from "./profilePictureStorage";
